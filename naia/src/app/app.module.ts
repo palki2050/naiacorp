b65cbd3c1b7fcec65acf79bd095f1a809274e37a
pooja goswami <poojagoswami2050@gmail.com>
2024-10-12 14:35:29 +0530

    installed ng-zorro-antd

--- naia/src/app/app.module.ts
+++ naia/src/app/app.module.ts
@@ -8,6 +8,15 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { AggridComponent } from './aggrid/aggrid.component';
 import { AgchartComponent } from './agchart/agchart.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -20,9 +29,14 @@ import { AgchartComponent } from './agchart/agchart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
